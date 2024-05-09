e59b9672784c2bb25f586484e3d750f3173177f6
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-09 21:10:40 +0530

    authenticate 2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -32,4 +32,97 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid password" });
+    }
+  } else {
+    res.status(401).json({ message: "User not found" });
+  }
+});
+
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logout successfully" });
+});
+
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Find the current user by ID
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    // Update user properties if provided in request body
+    if (username) user.username = username;
+    if (email) user.email = email;
+    if (password) {
+      // Hash the new password
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+      user.password = hashPassword;
+    }
+
+    // Save the updated user
+    await user.save();
+
+    // Return the updated user data
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    // If user not found, return 404 status and error message
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,37 @@
 import express from "express";
 
 //controllers
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+} from "../controllers/userController.js";
 //middlewares
+import {
+  authenticate,
+  authorizedAdmin,
+} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizedAdmin, getAllUsers);
+
+//login user
+router.post("/auth", loginUser);
+
+//logout
+router.post("/logout", logoutCurrentUser);
+
+//ptofile
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
 
 export default router;
